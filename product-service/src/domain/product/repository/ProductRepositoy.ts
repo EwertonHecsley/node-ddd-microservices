e46a424424bfd2667548930cf8cb306dc74e5632
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-04-21 16:52:31 -0300

    feat: routa para cadastrar produto

--- product-service/src/domain/product/repository/ProductRepositoy.ts
+++ product-service/src/domain/product/repository/ProductRepositoy.ts
@@ -3,7 +3,6 @@ import { Product } from "../entity/ProductEntity";
 export abstract class ProductRepository {
     abstract create(entity: Product): Promise<Product>;
     abstract findById(id: string): Promise<Product | undefined>;
-    abstract findByName(name: string): Promise<Product | undefined>;
     abstract findAll(): Promise<Product[]>;
     abstract delete(id: string): Promise<void>;
     abstract save(entity: Product): Promise<void>;
