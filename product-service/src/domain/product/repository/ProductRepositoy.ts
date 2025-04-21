78c9578d0954f15eb3a64b458d58c506222e3921
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-04-21 11:45:03 -0300

    feata: repository para Products

---
+++ product-service/src/domain/product/repository/ProductRepositoy.ts
@@ -0,0 +1,6 @@
+import { Product } from "../entity/ProductEntity";
+
+export abstract class ProductRepository {
+    abstract create(entity: Product): Promise<Product>;
+    abstract findById(id: string): Promise<Product | undefined>;
+}
